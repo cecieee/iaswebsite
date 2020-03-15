00b898a13be11f33410eb9efe566690f48aac468
Akhil Ravindran <imakhilravindran@gmail.com>
2020-03-15 15:55:20 +0530

    mobile menu implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ $('.carousel').carousel()
 
 $(document).ready(()=>{
 
+    // header resize on scroll
     window.onscroll = ()=> {
         if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50){
             $('header').addClass('scrolled')
@@ -9,5 +10,38 @@ $(document).ready(()=>{
             $('header').removeClass('scrolled')
         }
     }
+
+
+
+    // menu interaction
+    $('#menu-btn').click(function(){
+        $('.mobile-menu').show();
+        $('html, body').css({
+            overflow: 'hidden',
+            height: '100%'
+        });
+    });
+
+    $('.close-btn').click(function(){
+
+        $('.mobile-menu').hide();
+        $('html, body').css({
+            overflow: 'scroll',
+            height: '100%'
+        });
+    })
+
+    $('.mobile-menu li').click(function(e){
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        $('html, body').css({
+            overflow: 'scroll',
+            height: '100%'
+        });
+
+        $(window).scrollTo(0)
+    })
+    
     
 });
+
